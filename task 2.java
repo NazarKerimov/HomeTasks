9e6ffcdfec2f928fc582ee51653f6473bf422c32
Nazar Kerimov <defrin.nk@gmail.com>
2021-05-31 13:50:06 +0500

    task 2 complete

---
+++ task 2.java
@@ -0,0 +1,13 @@
+public class task_2 {
+    public static void main(String[] args) {
+        int morning = 7, evening = 20;
+
+        if (morning > 7 & evening < 20)
+            System.out.println("true");
+        else if (morning<7 & evening>20)
+            System.out.println("false");
+
+    }
+}
+
+// Я не разобрался с этой задачей
